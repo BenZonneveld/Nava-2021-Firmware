932a7357d730834954cd65aaa5d7df58511735ec
root <root@chrome.dyndns.org>
2021-06-19 02:57:18 +0200

    Modified the include for MemoryFree.h

--- Nava2021/features.h
+++ Nava2021/features.h
@@ -1,4 +1,4 @@
 // Feature switches:
-#define MIDI_BANK_PATTERN_CHANGE 0
-#define MIDI_DRUMNOTES_OUT       0
-#define MIDI_EXT_CHANNEL         0
+#define MIDI_BANK_PATTERN_CHANGE 1
+#define MIDI_DRUMNOTES_OUT       1
+#define MIDI_EXT_CHANNEL         1
