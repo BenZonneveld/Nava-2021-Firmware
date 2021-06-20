9243bdb0db71ef21504f8f34c1f6fe89ca1d4404
BenZonneveld <whilecount@gmail.com>
2021-06-20 17:34:37 +0200

    Revert pattern edit changes
    Reverted the pattern edit while not running.

--- Nava2021/Seq.ino
+++ Nava2021/Seq.ino
@@ -162,7 +162,6 @@ void SeqParameter()
     }
     if (curSeqMode != MUTE) muteBtn.counter = 0;
     if (!seq.configMode) seq.configPage = 0;
-
     if (tempoBtn.justRelease) needLcdUpdate = TRUE;
   }
 
@@ -265,7 +264,7 @@ void SeqParameter()
           curInst = OH;
           doublePush = 1;
         break;
-      }    
+      }
     }
     if (curInstChanged && stepsBtn.justRelease){
       needLcdUpdate = TRUE;
@@ -402,7 +401,57 @@ void SeqParameter()
     if (curSeqMode == PTRN_STEP){
       if (stepsBtn.justRelease) doublePush = FALSE; 
       if(!lastStepBtn.pressed && !instBtn && !keyboardMode && !shufBtn.pressed){                                      // [zabox] test
-        pattern[ptrnBuffer].inst[curInst] = InstValueGet(pattern[ptrnBuffer].inst[curInst]);//cf InstValueGet()
+        if (isRunning)
+        {
+          pattern[ptrnBuffer].inst[curInst] = InstValueGet(pattern[ptrnBuffer].inst[curInst]);//cf InstValueGet()
+        }
+        else if (!isRunning)
+        {//Return pattern number
+          if (stepsBtn.pressed){
+            if (bankBtn.pressed){
+              if(FirstBitOn() >= MAX_BANK) curBank = MAX_BANK;
+              else curBank = FirstBitOn();
+              nextPattern = curBank * NBR_PATTERN + (curPattern % NBR_PATTERN);
+              if(curPattern != nextPattern) selectedPatternChanged = TRUE;
+              group.length = 0;
+            }
+            else{//pattern group edit------------------------------------------------------
+              if (SecondBitOn())
+              {
+                trackPosNeedIncremante = FALSE;                                                               // [zabox] fixes group bug
+                group.length = SecondBitOn() - FirstBitOn();
+                nextPattern = group.firstPattern = FirstBitOn() + curBank * NBR_PATTERN;
+                doublePush = TRUE;
+                group.priority = TRUE;
+                //Store groupe in eeprom
+                if(enterBtn.justPressed){
+                  group.priority = FALSE;
+                  byte tempLength;
+                  byte tempPos;
+                  //Test if one the  selected pattern is already in a Group
+                  for (int a = 0; a <= group.length; a++){
+                    tempLength = LoadPatternGroup(group.firstPattern + a, LENGTH);
+                    if (tempLength){
+                      tempPos = LoadPatternGroup(group.firstPattern + a, POSITION);
+                      ClearPatternGroup(group.firstPattern + a - tempPos, tempLength);
+                    }
+                  }
+                  SavePatternGroup(group.firstPattern, group.length);
+                }
+              }
+              else if (!doublePush){
+                group.priority = FALSE;
+                nextPattern = FirstBitOn() + curBank * NBR_PATTERN;
+                if(enterBtn.justPressed){
+                  ClearPatternGroup(nextPattern - pattern[ptrnBuffer].groupPos, pattern[ptrnBuffer].groupLength);
+                  group.length = 0;
+                }
+                group.pos = pattern[ptrnBuffer].groupPos;
+              }
+              if(curPattern != nextPattern) selectedPatternChanged = TRUE;
+            }
+          }
+        }
       }
     }
     //////////////////////////////TAP EDIT///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -819,9 +868,6 @@ void SeqParameter()
     //////////////////////////MODE TRACK PLAY/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   if (curSeqMode == TRACK_PLAY)
   {
-    Serial.print("curSeqMode: "); Serial.println(curSeqMode);
-    Serial.print("prevSeqMode: "); Serial.println(prevSeqMode);
-    
     if (trackPosNeedIncremante){//(endMeasure)
       trk.pos++;
       if (trk.pos >= track[trkBuffer].length) trk.pos = 0;
